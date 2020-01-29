900a6dd1ff248cf3f6629b38f2623564bdd4fb22
Pam Cairo <pcairo_90@outlook.com>
2020-01-29 04:16:53 +0000

    Automatically backed up by Learn

---
+++ lib/email_parser.rb
@@ -0,0 +1,14 @@
+class EmailParser
+	attr_accessor :emails
+	
+	def initialize(emails)
+		@emails= emails
+	end
+	
+	def parse
+		@emails = @emails.gsub(/[\s,]/ ," ").split
+	
+		@emails.uniq
+	end
+	
+end
